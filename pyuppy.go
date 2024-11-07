6fd90f1bdc32f0b96eebf0b1c0a08eae7abbcefa
Wassup Chicken <74932143+wassup-chicken@users.noreply.github.com>
2024-11-07 10:05:30 -0500

    third

--- pyuppy.go
+++ pyuppy.go
@@ -1,6 +1,8 @@
 package puppy
 
 import (
+	"fmt"
+
 	"github.com/wassup-chicken/dog"
 )
 
@@ -18,4 +20,15 @@ func BigBark() string {
 
 func BigBarks() string {
 	return dog.WhenGrownUp(Barks())
+}
+
+func From11() {
+	fmt.Println("I'm from version 1.1.0")
+}
+
+func From12() {
+	fmt.Println("I'm from verision 1.2.0")
+}
+func From13() {
+	fmt.Println("I'm from verision 1.3.0")
 }
